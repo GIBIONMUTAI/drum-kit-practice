e171a86e6fb4e4151087e1cc3d702d65a6287bd9
GIBIONMUTAI <135413010+GIBIONMUTAI@users.noreply.github.com>
2023-11-16 07:49:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+
+var numberOfDrumButtons=this.innerHTML;
+switch (buttonInnerHTML) {
+    case "w":
+        var audio=new audio("sounds/tom-1.mp3")
+        audio.play();
+        break;
+        
+            case "a":
+                var audio=new audio("sounds/tom-2.mp3")
+                audio.play();
+                break;
+                
+                    case "s":
+                        var audio=new audio("sounds/tom-3.mp3")
+                        audio.play();
+                        break;
+                        
+                            case "d":
+                                var audio=new audio("sounds/tom-4.mp3")
+                                audio.play();
+                                break;
+                                
+                                    case "j":
+                                        var audio=new audio("sounds/tom-5.mp3")
+                                        audio.play();
+                                        break;
+                                        
+                                            case "k":
+                                                var audio=new audio("sounds/tom-6.mp3")
+                                                audio.play();
+                                                break;
+                                                
+                                                    case "l":
+                                                        var audio=new audio("sounds/tom-7.mp3")
+                                                        audio.play();
+                                                        break;
+
+    default:console.log(innerHTML);
+        
+}
+
+
